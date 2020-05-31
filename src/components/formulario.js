a834439dd205ab5b9593295802e036469c743923
yeraldith <yeraldith.camargo@gmail.com>
2020-05-30 20:18:38 -0500

    Modifique fondo de formulario

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Seccion from "./seccion";
+import '../style/formualrio.css'
 
 const Formulario = ({ variables, secciones}) => {
   return (
-    <form id="informacion" className="container mt-5 mb-5">
+    <form id="informacion" className="container mt-5 mb-5 formulario">
+       <h1 className="text-center mt-5">
+         Ingresar Datos
+       </h1>
       <div className="row">
         {secciones.map((item, index) => {
           return (
             <section
               key={"Seccion" + index}
-              className="col-12 col-lg-4 mt-3 bg-light"
+              className="col-12 col-lg-4 mt-3"
             >
               <div className="rounded-pill p-2">
-                <h1 className="text-center text-info mt-5 mb-5">
+                <h3 className="text-center text-info mt-5 mb-5">
                   {item.nombre}
-                </h1>
+                </h3>
               </div>
               <Seccion variables={variables} valor={item.valor} />
             </section>
